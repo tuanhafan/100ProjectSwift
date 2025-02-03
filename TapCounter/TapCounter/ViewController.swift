a025e8166b7faba4831c81e501afbf94291145be
tuanhafan <tuanhafan@gmail.com>
2025-02-03 16:09:04 +0700

    uopdate tap counter

--- TapCounter/TapCounter/ViewController.swift
+++ TapCounter/TapCounter/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbCounter: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    var number :Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetCount))
+        lbCounter.text = "\(number)"
     }
 
-
+    @IBAction func tapAction(_ sender: Any) {
+        number = number + 1
+        lbCounter.text = "\(number)"
+    }
+    @objc func resetCount(){
+        number = 0
+        lbCounter.text = "\(number)"
+    }
 }
 

7d19f67172d230a9828c1b0d3f27065fdeb4da4b
tuanhafan <tuanhafan@gmail.com>
2025-02-05 15:18:42 +0700

    update currentDate

--- CurrenDateTime/CurrenDateTime/ViewController.swift
+++ CurrenDateTime/CurrenDateTime/ViewController.swift
@@ -12,42 +12,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbShowDateTimer: UILabel!
     @IBOutlet weak var lbinFor: UILabel!
     var timer:Timer?
-    let getDate = Date()
-    let calender = Calendar.current
-    var text:String = ""
-    var step:Int = 5
-    var currentSc : Int = 5
+    var currentDate = Date()
+    var step = 15
+    @IBOutlet weak var btnResfresh: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       let dateComponent = calender.dateComponents([.year,.month,.day], from: getDate)
-       let dateFormatter = DateFormatter()
-       dateFormatter.dateFormat = "MMM"
-        text = "\(dateFormatter.string(from: getDate)) \(dateComponent.day!), \(dateComponent.year!), "
+        
+       
         startTimer()
     }
     func startTimer(){
         updateClock()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
     }
-
+    
     @objc func updateClock() {
-        let timeFormatter = DateFormatter()
-        let hour = calender.component(.hour, from: getDate)
-        let minute = calender.component(.minute, from: getDate)
-        let second = Calendar.current.component(.second, from: Date())
-        timeFormatter.dateFormat = "HH:mm:ss"
-        
-        currentSc = second + 5
-        if(currentSc >= 60){
-            currentSc = 1
+//
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: currentDate)
+        if let minute = dateComponents.minute, let second = dateComponents.second {
+            let newSeconds = second + step
+            
+            if(newSeconds >= 60) {
+               let exTraMinute = newSeconds / 60
+               let reMainingSecond = newSeconds % 60
+                
+                dateComponents.minute = minute + exTraMinute
+                dateComponents.second = reMainingSecond
+            }
+            else {
+                dateComponents.second = newSeconds
+            }
         }
-        print(currentSc)
-        lbShowDateTimer.text = "\(text)\(String(format: "%2d:%2d:%2d", hour,minute,currentSc))"
+        if let newCurrent = Calendar.current.date(from: dateComponents){
+            currentDate = newCurrent
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, yyyy, HH:mm:ss"
+        lbShowDateTimer.text = formatter.string(from: currentDate)
     }
     deinit {
         timer?.invalidate()
         timer = nil
+    }
+    
+    @IBAction func refreshAction(_ sender: Any) {
+        step = (step == 15) ? 1 : 15
     }
 }
 

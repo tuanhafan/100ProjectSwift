80175316097cfc53e9f0f940dd7c0ea738c4835e
tuanhafan <tuanhafan@gmail.com>
2025-02-13 18:01:50 +0700

    update 2 tables

--- CreditBalance/CreditBalance/CustomCell.swift
+++ CreditBalance/CreditBalance/CustomCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class CustomCell: UITableViewCell {
 
+    
+    
+    
+    
     let viewAva : UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.lightGray.withAlphaComponent(0.2)
@@ -49,11 +53,50 @@ class CustomCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    let viewCellHeader : UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 12
+        view.clipsToBounds = true
+        view.backgroundColor = .systemBlue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-    
+    let lblTitleHeader: UILabel = {
+         let label = UILabel()
+         label.text = "Credit Balance"
+         label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+         label.textColor = .white
+         return label
+     }()
 
+      let lblBalanceHeader: UILabel = {
+         let label = UILabel()
+         label.text = "€32,215"
+         label.font = UIFont.boldSystemFont(ofSize: 48)
+         label.textColor = .white
+         return label
+     }()
+
+      let lblTrendHeader: UILabel = {
+         let label = UILabel()
+         label.text = "⬇ 4.2%"
+         label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+         label.textColor = .white
+         label.textAlignment = .center
+         label.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+         label.layer.cornerRadius = 5
+         return label
+     }()
+
+      let waveImageViewHeader: UIImageView = {
+         let imageView = UIImageView()
+         imageView.image = UIImage(named: "wavesIcon")
+         imageView.tintColor = .white
+         return imageView
+     }()
+
+    func setUpCellContent() {
         viewAva.addSubview(avatar)
         stackView.addArrangedSubview(lblName)
         stackView.addArrangedSubview(lblTime)
@@ -63,6 +106,9 @@ class CustomCell: UITableViewCell {
         contentView.addSubview(lblBalance)
         
         NSLayoutConstraint.activate([
+            
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 60),
+
             
             viewAva.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             viewAva.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
@@ -86,13 +132,63 @@ class CustomCell: UITableViewCell {
         ])
         
     }
+    func setUpHeaderCell() {
+       
+        
+        
+        viewCellHeader.addSubview(lblTitleHeader)
+        viewCellHeader.addSubview(lblBalanceHeader)
+        viewCellHeader.addSubview(lblTrendHeader)
+        viewCellHeader.addSubview(waveImageViewHeader)
+        contentView.addSubview(viewCellHeader)
+
+        lblTitleHeader.translatesAutoresizingMaskIntoConstraints = false
+        lblBalanceHeader.translatesAutoresizingMaskIntoConstraints = false
+        lblTrendHeader.translatesAutoresizingMaskIntoConstraints = false
+        waveImageViewHeader.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            viewCellHeader.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            viewCellHeader.heightAnchor.constraint(equalToConstant: 150),
+            
+            lblTitleHeader.topAnchor.constraint(equalTo: viewCellHeader.topAnchor, constant: 20),
+            lblTitleHeader.leadingAnchor.constraint(equalTo: viewCellHeader.leadingAnchor, constant: 30),
+
+            lblBalanceHeader.topAnchor.constraint(equalTo: lblTitleHeader.bottomAnchor, constant: 15),
+            lblBalanceHeader.leadingAnchor.constraint(equalTo: viewCellHeader.leadingAnchor, constant: 30),
+
+            lblTrendHeader.topAnchor.constraint(equalTo: viewCellHeader.topAnchor, constant: 22),
+            lblTrendHeader.trailingAnchor.constraint(equalTo: viewCellHeader.trailingAnchor, constant: -22),
+            lblTrendHeader.widthAnchor.constraint(equalToConstant: 60),
+            lblTrendHeader.heightAnchor.constraint(equalToConstant: 30),
+
+            waveImageViewHeader.trailingAnchor.constraint(equalTo: viewCellHeader.trailingAnchor, constant: -22),
+            waveImageViewHeader.bottomAnchor.constraint(equalTo: viewCellHeader.bottomAnchor, constant: -22),
+            waveImageViewHeader.widthAnchor.constraint(equalToConstant: 80),
+            waveImageViewHeader.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+   
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    func configure(recent item:Recent){
-        avatar.image = UIImage(systemName: "\(item.avatar)")
-        lblName.text = "\(item.name)"
-        lblTime.text = "\(item.time)"
-        lblBalance.text = "€\(item.balance)"
+    func configure(recent item:Recent? = nil,typeCell:String){
+        if(typeCell ==  "Content"){
+            setUpCellContent()
+            avatar.image = UIImage(systemName: "\(item!.avatar)")
+            lblName.text = "\(item!.name)"
+            lblTime.text = "\(item!.time)"
+            lblBalance.text = "€\(item!.balance)"
+        } else {
+            setUpHeaderCell()
+        }
+       
     }
 }
--- CreditBalance/CreditBalance/ViewController.swift
+++ CreditBalance/CreditBalance/ViewController.swift
@@ -22,18 +22,22 @@ let recents:[Recent] = [
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var viewHeader: ViewHeader!
+    @IBOutlet weak var tableViewHeader: UITableView!
     
     @IBOutlet weak var lblrecent: UILabel!
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewContent: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.register(CustomCell.self, forCellReuseIdentifier: "cell")
-        tableView.rowHeight = 60
-        tableView.separatorStyle = .none
+        tableViewContent.register(CustomCell.self, forCellReuseIdentifier: "cell")
+        tableViewHeader.register(CustomCell.self, forCellReuseIdentifier: "cellHeader")
+        tableViewContent.rowHeight = 60
+        tableViewContent.separatorStyle = .none
+        tableViewHeader.rowHeight = UITableView.automaticDimension
+        
+        
     }
     
     
@@ -45,95 +49,32 @@ extension ViewController : UITableViewDelegate,UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recents.count
+        if(tableView == tableViewContent) {
+            return recents.count
+        } else {
+            return 1
+        }
+        
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCell else {
-           print("vaof dday")
-            return UITableViewCell()
+        if(tableView == tableViewContent)
+        {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCell else {
+              
+                return UITableViewCell()
+        }
+            let recent:Recent = recents[indexPath.row]
+            cell.configure(recent: recent,typeCell: "Content")
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellHeader", for: indexPath) as? CustomCell else {
+              
+                return UITableViewCell()
+        }
+            
+            cell.configure(typeCell: "Header")
+            return cell
+        }
+        
     }
-        let recent:Recent = recents[indexPath.row]
-        cell.configure(recent: recent)
-        return cell
-    }
-}
-
-class ViewHeader : UIView {
-    
-     let lblTitle: UILabel = {
-          let label = UILabel()
-          label.text = "Credit Balance"
-          label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
-          label.textColor = .white
-          return label
-      }()
-
-       let lblBalance: UILabel = {
-          let label = UILabel()
-          label.text = "€32,215"
-          label.font = UIFont.boldSystemFont(ofSize: 48)
-          label.textColor = .white
-          return label
-      }()
-
-       let lblTrend: UILabel = {
-          let label = UILabel()
-          label.text = "⬇ 4.2%"
-          label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
-          label.textColor = .white
-          label.textAlignment = .center
-          label.backgroundColor = UIColor.white.withAlphaComponent(0.2)
-          label.layer.cornerRadius = 5
-          return label
-      }()
-
-       let waveImageView: UIImageView = {
-          let imageView = UIImageView()
-          imageView.image = UIImage(named: "wavesIcon")
-          imageView.tintColor = .white
-          return imageView
-      }()
-
-      override init(frame: CGRect) {
-          super.init(frame: frame)
-          setupView()
-      }
-
-      required init?(coder: NSCoder) {
-          super.init(coder: coder)
-          setupView()
-      }
-
-      private func setupView() {
-          self.layer.cornerRadius = 12
-          self.clipsToBounds = true
-          self.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 40, leading: 40, bottom: 40, trailing: 40)
-          addSubview(lblTitle)
-          addSubview(lblBalance)
-          addSubview(lblTrend)
-          addSubview(waveImageView)
-
-          lblTitle.translatesAutoresizingMaskIntoConstraints = false
-          lblBalance.translatesAutoresizingMaskIntoConstraints = false
-          lblTrend.translatesAutoresizingMaskIntoConstraints = false
-          waveImageView.translatesAutoresizingMaskIntoConstraints = false
-
-          NSLayoutConstraint.activate([
-                lblTitle.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
-                lblTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
-
-                lblBalance.topAnchor.constraint(equalTo: lblTitle.bottomAnchor, constant: 15),
-                lblBalance.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
-
-                lblTrend.topAnchor.constraint(equalTo: self.topAnchor, constant: 22),
-                lblTrend.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -22),
-                lblTrend.widthAnchor.constraint(equalToConstant: 60),
-                lblTrend.heightAnchor.constraint(equalToConstant: 30),
-
-                waveImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -22),
-                waveImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -22),
-                waveImageView.widthAnchor.constraint(equalToConstant: 80),
-                waveImageView.heightAnchor.constraint(equalToConstant: 30)
-          ])
-      }
 }

d15d578edc8e90c039b481e0d6f7c0c703a51d77
tuanhafan <tuanhafan@gmail.com>
2025-02-21 21:29:10 +0700

    crate tableView and search bar item

---
+++ MoviesWithTableView/MoviesWithTableView/Data.swift
@@ -0,0 +1,35 @@
+//
+//  Data.swift
+//  MoviesWithTableView
+//
+//  Created by AlexTran on 21/02/2025.
+//
+
+import Foundation
+struct Movie {
+    let imageName: String
+    let title: String
+}
+
+let movies: [Movie] = [
+    Movie(imageName: "0", title: "The Shawshank Redemption"),
+    Movie(imageName: "1", title: "The Godfather"),
+    Movie(imageName: "2", title: "The Dark Knight"),
+    Movie(imageName: "3", title: "Pulp Fiction"),
+    Movie(imageName: "4", title: "Forrest Gump"),
+    Movie(imageName: "5", title: "Inception"),
+    Movie(imageName: "6", title: "Fight Club"),
+    Movie(imageName: "7", title: "The Matrix"),
+    Movie(imageName: "8", title: "Interstellar"),
+    Movie(imageName: "9", title: "The Green Mile"),
+    Movie(imageName: "0", title: "The Silence of the Lambs"),
+    Movie(imageName: "1", title: "Saving Private Ryan"),
+    Movie(imageName: "2", title: "Schindler's List"),
+    Movie(imageName: "3", title: "The Prestige"),
+    Movie(imageName: "4", title: "The Lion King"),
+    Movie(imageName: "5", title: "Gladiator"),
+    Movie(imageName: "6", title: "Titanic"),
+    Movie(imageName: "7", title: "Avengers: Endgame"),
+    Movie(imageName: "8", title: "Joker"),
+    Movie(imageName: "9", title: "Django Unchained")
+]
---
+++ MoviesWithTableView/MoviesWithTableView/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  MoviesWithTableView
+//
+//  Created by AlexTran on 21/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableContent: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    var data : [Movie] = movies
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+   
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].title
+        return cell
+    }
+}
+extension ViewController : UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        var newData  = movies.filter {$0.title.lowercased().contains(searchText.lowercased())}
+        if newData.isEmpty && searchText.isEmpty {
+            newData  = movies
+        }
+        else if(newData.isEmpty){
+            newData  = movies.filter {$0.title.lowercased().contains(searchText.lowercased())}
+        }
+        data = newData
+        tableContent.reloadData()
+    }
+}

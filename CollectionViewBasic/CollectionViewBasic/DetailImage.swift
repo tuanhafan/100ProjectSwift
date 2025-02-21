6ba21f569375760ce751fe66b97c54ea8d5dffde
tuanhafan <tuanhafan@gmail.com>
2025-02-21 16:06:36 +0700

    update alert action to collectionView

--- CollectionViewBasic/CollectionViewBasic/DetailImage.swift
+++ CollectionViewBasic/CollectionViewBasic/DetailImage.swift
@@ -15,7 +15,19 @@ class DetailImage: UIViewController {
         super.viewDidLoad()
         imageContent.image = UIImage(named: nameImage)
         imageContent.contentMode = .scaleAspectFill
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, target: self, action: #selector(alertAction))
+        
+        
+        
     }
 
-
+    @objc func alertAction(){
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Report", style: .destructive, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Share", style: .default, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Save Image", style: .default, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Coppy Link", style: .default,handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(actionSheet,animated: true,completion: nil)
+    }
 }
